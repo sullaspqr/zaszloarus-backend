7a164a2632c3abe9dfdbf624c535f5a260be9062
sullaspqr <sulla.spqr78@gmail.com>
2022-03-21 11:47:30 +0100

    first commit

---
+++ ZaszloArusAPI_EF/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZaszloArusAPI_EF.Models;
+
+namespace ZaszloArusAPI_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            List<Product> products = new List<Product>();
+            using (var context = new zaszloarusContext())
+            {
+                try
+                {
+                    return new JsonResult(context.Products.ToList());
+                }
+                catch (System.Exception ex)
+                {
+                    return new JsonResult(ex.Message);
+                }
+            }
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Product product)
+        {
+            using (var context = new zaszloarusContext())
+            {
+                try
+                {
+                    context.Products.Add(product);
+                    context.SaveChanges();
+                    return new JsonResult("Termék felvétele sikeresen megtörtént.");
+                }
+                catch (Exception ex)
+                {
+                    return new JsonResult(ex.Message);
+                }
+            }
+        }
+
+        [HttpPut]
+
+        public JsonResult Put(Product product)
+        {
+            using (var context=new zaszloarusContext())
+            {
+                try
+                {
+                    context.Products.Update(product);
+                    context.SaveChanges();
+                    return new JsonResult("Az adatok módosítása megtörtént");
+                }
+                catch (Exception ex)
+                {
+                    return new JsonResult(ex.Message);
+                }
+            }
+        }
+
+        [HttpDelete]
+
+        public JsonResult Delete(int id)
+        {
+            using (var context= new zaszloarusContext())
+            {
+                try
+                {
+                    Product product = new Product();
+                    product.Id = id;
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+                    return new JsonResult("Törlés sikeresen megtörtént");
+                }
+                catch (Exception ex)
+                {
+                    return new JsonResult(ex.Message);
+                }
+            }
+        }
+    }
+}
